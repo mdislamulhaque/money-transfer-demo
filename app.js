c8b7be530f71d6b73ec1b2c45c2df81d15ff9f18
Meherul Meher <mehrul.islam@vivatelecoms.com>
2025-03-19 12:15:37 +0600

    add login and sign up page

--- app.js
+++ app.js
@@ -2,11 +2,31 @@ const mobileMenuButton = document.getElementById("mobile-menu-button");
 const mobileMenu = document.getElementById("mobile-menu");
 const mainMenu = document.getElementById("mainMenu");
 
-
 mobileMenuButton.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
 });
 
 mainMenu.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
+});
+
+function toggleFAQ(id) {
+  const answer = document.getElementById(`answer-${id}`);
+  const icon = document.getElementById(`icon-${id}`);
+
+  answer.classList.toggle("hidden");
+  icon.classList.toggle("rotate-180");
+}
+
+let lastScrollY = 0;
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    navbar.classList.add("visible-nav");
+    navbar.classList.remove("hidden-nav");
+  } else {
+    navbar.classList.add("hidden-nav");
+    navbar.classList.remove("visible-nav");
+  }
 });
